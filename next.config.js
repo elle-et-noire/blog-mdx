333f4a93c29e6011a2872412150967a07981d135
Lomega <atibon3@gmail.com>
2023-09-29 18:31:42 +0900

    next config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
+
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  assetPrefix: branchName,
+  basePath: branchName,
+};
+
+module.exports = nextConfig;
